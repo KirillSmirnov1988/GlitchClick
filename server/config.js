aafa0cdd015e13ff8c4942fcccae77088cc7c7b3
KirillSmirnov1988 <83866020+KirillSmirnov1988@users.noreply.github.com>
2025-03-02 08:15:46 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,8 +1,9 @@
 module.exports = {
-	redis: {
-		host: "redis-19073.c80.us-east-1-2.ec2.redns.redis-cloud.com",
-		port: 19073,
-		username: "default",
-		password: "NphMui13jIF1DNHaKPoDRLAlNSWsO6YB",
-	},
+    redis: {
+        host: process.env.REDIS_HOST,           
+        port: process.env.REDIS_PORT,            
+        username: process.env.REDIS_USERNAME,    
+        password: process.env.REDIS_PASSWORD,    
+    }
 };
+
